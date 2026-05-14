9daeeaf5e332d6ae645239d20a0d5f9648eb24ea
Daouda-sudo <ndaw.daoudadibocor.studente@itispaleocapa.it>
2026-05-14 05:23:26 +0000

    implementazione lista in userModel.js

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,34 +1,41 @@
 // app/models/userModel.js
-let users = [];
+let users = {};
+
 const User = {
-  findAll: () => users,
+  findAll: () => Object.values(users),
+
   create: (name, email, imgLink, prezzo, numero) => {
-    const newUser = { id: Date.now(), name, email, imgLink, prezzo, numero };
-    users.push(newUser);
+    const id = Date.now();
+    const newUser = { 
+      id, 
+      name, 
+      email, 
+      imgLink, 
+      prezzo, 
+      numero: parseInt(numero) || 0 
+    };
+    
+    users[id] = newUser; 
     return newUser;
   },
-  findById: (id) => users.find(u => u.id === parseInt(id)),
-  delete: (id) => {
-    users = users.filter(u => u.id !== parseInt(id));
-  },
-  addOne: (id) => {
-    for(let i = 0; i<users.length; i++){
-       if(users[i].id ==  parseInt(id)){
-        let n = parseInt(users[i].numero)
-        users[i].numero = n+1;
-      }
-    }
-  },
-  subtractOne: (id) => {
-    for(let i = 0; i<users.length; i++){
-       if(users[i].id ==  parseInt(id)){
-        let n = parseInt(users[i].numero)
-        users[i].numero = n-1;
-      }
 
+  findById: (id) => users[id],
+
+  delete: (id) => {
+    delete users[id];
+  },
+
+  addOne: (id) => {
+    if (users[id]) {
+      users[id].numero += 1;
     }
   },
-  
+
+  subtractOne: (id) => {
+    if (users[id]) {
+      users[id].numero -= 1;
+    }
+  },
 };
 
 module.exports = User;
